1109b2c3321dea6aace01c1d235edf1eb892f7b2
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-17 16:17:03 +0100

    change back end

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const QUERY = gql`
-  query Countries {
-    countries {
-      code
+  query Users {
+    allUsers {
+      id
       name
     }
   }
